d339c45a191613463c0ec71e2a975cd6cef509a2
naanas <annasputra52@gmail.com>
2026-01-27 16:29:57 +0700

    enhance

--- src/components/BackgroundEffects.tsx
+++ src/components/BackgroundEffects.tsx
@@ -7,9 +7,9 @@ export default function BackgroundEffects() {
             <div className="absolute inset-0 bg-grid-pattern opacity-30"></div>
 
             {/* Animated Blobs - Responsive Sizes */}
-            <div className="absolute top-0 -left-4 w-64 h-64 md:w-96 md:h-96 bg-[var(--dark-blood)] rounded-full mix-blend-multiply filter blur-3xl opacity-20 animate-blob"></div>
-            <div className="absolute top-0 -right-4 w-64 h-64 md:w-96 md:h-96 bg-[var(--blood-red)] rounded-full mix-blend-multiply filter blur-3xl opacity-20 animate-blob animation-delay-2000"></div>
-            <div className="absolute -bottom-32 left-20 w-64 h-64 md:w-96 md:h-96 bg-[var(--void-black)] rounded-full mix-blend-multiply filter blur-3xl opacity-30 animate-blob animation-delay-4000 border border-[var(--dark-blood)]/20"></div>
+            <div className="absolute top-0 -left-4 w-64 h-64 md:w-96 md:h-96 bg-[var(--dark-blood)] rounded-full mix-blend-multiply filter blur-3xl opacity-20 animate-blob will-change-transform"></div>
+            <div className="absolute top-0 -right-4 w-64 h-64 md:w-96 md:h-96 bg-[var(--blood-red)] rounded-full mix-blend-multiply filter blur-3xl opacity-20 animate-blob animation-delay-2000 will-change-transform"></div>
+            <div className="absolute -bottom-32 left-20 w-64 h-64 md:w-96 md:h-96 bg-[var(--void-black)] rounded-full mix-blend-multiply filter blur-3xl opacity-30 animate-blob animation-delay-4000 border border-[var(--dark-blood)]/20 will-change-transform"></div>
 
             {/* Center Glow */}
             <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 w-[300px] h-[300px] md:w-[800px] md:h-[800px] bg-[radial-gradient(circle,rgba(60,4,11,0.2)_0%,transparent_70%)] pointer-events-none"></div>
--- src/lib/api.ts
+++ src/lib/api.ts
@@ -7,6 +7,7 @@ const api = axios.create({
     headers: {
         'Content-Type': 'application/json',
     },
+    timeout: 15000, // 15 seconds timeout
 });
 
 // Request Logger & Auth Token
